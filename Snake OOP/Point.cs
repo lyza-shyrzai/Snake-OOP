adc807a14939a7055ae76cb32a6022b14895fb4f
LizaSh <lizash605@gmail.com>
2016-09-29 22:08:31 +0300

    Создаем конструкторы класса Point и используем инкапсуляцию

--- Snake OOP/Point.cs
+++ Snake OOP/Point.cs
@@ -12,6 +12,19 @@ namespace Snake_OOP
         public int y;
         public char sym;
 
+        public Point()
+        {
+                      
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
--- Snake OOP/Program.cs
+++ Snake OOP/Program.cs
@@ -10,16 +10,10 @@ namespace Snake_OOP
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 3;
-            p1.y = 1;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 6;
-            p2.y = 4;
-            p2.sym = '$';
+            Point p2 = new Point(4, 6, '$');          
             p2.Draw();
 
             Console.ReadLine();
