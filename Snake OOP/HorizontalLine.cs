2d611637dfc450316dc2cbda59eaa42b1730da96
LizaSh <lizash605@gmail.com>
2016-09-30 22:24:01 +0300

    Создали класс HorizontalLine

---
+++ Snake OOP/HorizontalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_OOP
+{
+    class HorizontalLine
+    {
+        List<Point> plist;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            plist = new List<Point>();
+            for (int x = xLeft; x<= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                plist.Add(p);
+            }           
+        }
+
+        public void Drow()
+        {
+            foreach(Point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- Snake OOP/Program.cs
+++ Snake OOP/Program.cs
@@ -16,35 +16,8 @@ namespace Snake_OOP
             Point p2 = new Point(4, 6, '$');          
             p2.Draw();
 
-            List<int> numList = new List<int>();
-                numList.Add(4);
-                numList.Add(6);
-                numList.Add(3);
-                numList.Add(8);
-
-                int x = numList[0];
-                int y = numList[1];
-                int z = numList[2];
-                int t = numList[3];
-
-                Console.WriteLine(" ");
-
-                foreach ( int i in numList)
-                {
-                    Console.WriteLine(i);
-                }
-
-            List<char> chList = new List<char>();
-                chList.Add('%');
-                char r = chList[0];
-
-                Console.WriteLine(" ");
-                
-                for (int i = 0; i < 5; i++)
-                {
-                    Console.WriteLine(r);
-                }
-
+            HorizontalLine line = new HorizontalLine(5, 50, 10, '@');
+            line.Drow();
 
 
             Console.ReadLine();
